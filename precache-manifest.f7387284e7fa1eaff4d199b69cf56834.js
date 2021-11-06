8b10044094c02bd04822e33e9e92979230599679
Lin <hailin_zhu2018@126.com>
2021-11-06 17:40:29 +0800

    deploy

---
+++ precache-manifest.f7387284e7fa1eaff4d199b69cf56834.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c0bb471bbd2b129a11",
+    "url": "/morney-website/css/app.e886290e.css"
+  },
+  {
+    "revision": "5b7770dd9fc9c80f1ee91aeb7606470b",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "13c0bb471bbd2b129a11",
+    "url": "/morney-website/js/app.9323b557.js"
+  },
+  {
+    "revision": "b570e8ce5a762a61aa80",
+    "url": "/morney-website/js/chunk-vendors.21cf013d.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "f03e28e360fa86d872b2c2dd2bd32006",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
